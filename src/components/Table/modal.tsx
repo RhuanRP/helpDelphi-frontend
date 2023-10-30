dec2738d5abb90e3d512df9624b4c690f26f698c
Rhuan Pablo <86031472+RhuanRP@users.noreply.github.com>
2023-10-29 22:43:32 -0300

    Complementando o modal

--- src/components/Table/modal.tsx
+++ src/components/Table/modal.tsx
@@ -1,53 +1,79 @@
 // Modal.tsx
-import React from "react";
+import React, { useState } from "react";
+import "./modal.css";
+import { Button } from "../Button";
 
 type ModalProps = {
   // 
 };
 
 const Modal: React.FC<ModalProps> = (props) => {
+  const [showPopup, setShowPopup] = useState(false);
+
   return (
     <div className="modal">
       <div className="modal-content">
-        <label>
-          <p>Cliente</p>
-          <input type="text-client"  />
-          </label>
-          
-          <label>
-          Status
-          <select>
-              <option value="aberto">Aberto</option>
-              <option value="em-andamento">Em andamento</option>
-              <option value="fechado">Fechado</option>
-            </select>
-        </label>
+        <div className="modal-inputs">
+          <div className="modal-left">
+            <label>
+              <p>Cliente</p>
+              <input type="text-client" />
+            </label>
 
-        <label>
-          <p>Assunto</p>
-          <input type="text-subject"  />
-          </label>
-          
-          <label>Criticidade
-          <select>
-              <option value="baixa">Baixa</option>
-              <option value="media">Média</option>
-              <option value="alta">Alta</option>
-            </select>
-        </label>
+            <label>
+              <p>Assunto</p>
+              <input type="text-subject" />
+            </label>
+          </div>
+
+          <div className="modal-right">
+            <label>
+              Status
+              <select onChange={(e) => setShowPopup(e.target.value === "pausado")}>
+                <option value="aguardando">Aguardando</option>
+                <option value="em-andamento">Em andamento</option>
+                <option value="pausado">Pausado</option>
+                <option value="resolvido">Resolvido</option>
+              </select>
+            </label>
+            <label>Criticidade
+              <select>
+                <option value="baixa">Baixa</option>
+                <option value="media">Média</option>
+                <option value="alta">Alta</option>
+              </select>
+            </label>
+          </div>
+        </div>
 
         <label className="text-description">
-          Descrição:
-          <textarea/>
+          Descrição
+          <textarea />
         </label>
 
         <label className="historic-actions">
           Histórico de Ações
-          <textarea/>
+          <textarea />
           <ul>
-            {}
+            { }
           </ul>
         </label>
+        <div className="button-action">
+          <Button type="button">Salvar</Button>
+          <Button type="button">Cancelar</Button>
+        </div>
+        {showPopup && (
+          <div className="popup">
+            <label>
+              Motivo da Pausa
+              <input type="text" />
+            </label>
+            <div className="button-action">
+              <Button type="button" onClick={() => setShowPopup(true)}>Salvar</Button>
+              <Button type="button" onClick={() => setShowPopup(false)}>Fechar</Button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
