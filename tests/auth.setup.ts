24d85c22b9a1b2cb7c6be14151550d205503a73e
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2023-11-03 14:35:43 -0300

    feat: tests playwright

---
+++ tests/auth.setup.ts
@@ -0,0 +1,16 @@
+import { test as setup, expect } from "@playwright/test";
+
+const authFile = "playwright/.auth/user.json";
+
+setup("authenticate", async ({ page }) => {
+  await page.goto("/login");
+  await page.locator("input[name=username]").fill("samuelfr");
+  await page.locator("input[name=password]").fill("123");
+  await page.getByText("Acessar").click();
+
+  await page.waitForURL("/");
+
+  await expect(page.getByText("Assunto")).toBeVisible();
+
+  await page.context().storageState({ path: authFile });
+});
