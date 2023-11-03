4825237bd85fec8356eacc4f09106344ed0ad303
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2023-11-02 21:34:31 -0300

    fix: case sensitive

---
+++ src/Login2.tsx
@@ -0,0 +1,100 @@
+import { FormEvent } from "react";
+import logo from "./assets/logo.png";
+import { Button } from "./components/Button";
+import "./styles/Login.css";
+import { Input } from "./components/Form/Input";
+import { useMutation } from "@tanstack/react-query";
+import { z } from "zod";
+import { authSchema } from "./lib/validations/authSchema";
+import { api } from "./lib/api";
+import { useAuth } from "./hooks/useAuth";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import React from "react";
+import { catchError } from "./lib/utils";
+
+type Input = z.infer<typeof authSchema>;
+
+type SignInResponse = {
+  user: {
+    username: string;
+    role: "admin" | "client" | "technician";
+  };
+  token: string;
+};
+
+const Login = () => {
+  const [user, setUser] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const { signIn } = useAuth();
+  const [cookies] = useCookies(["helpdelphi_api_token"]);
+  const navigate = useNavigate();
+
+  const mutation = useMutation({
+    mutationFn: async (input: Input) => {
+      const { data } = await api.post<SignInResponse>("/users/session", input);
+      return data;
+    },
+    onSuccess: async (data) => {
+      try {
+        await signIn({
+          username: data.user.username,
+          token: data.token,
+          role: data.user.role,
+        });
+      } catch (error) {
+        catchError(error);
+      }
+    },
+    onError: (err) => {
+      console.log(err);
+    },
+  });
+
+  function onSubmit(e: FormEvent) {
+    e.preventDefault();
+    mutation.mutate({
+      username: user,
+      password,
+    });
+  }
+
+  React.useEffect(() => {
+    if (typeof cookies.helpdelphi_api_token === "string") {
+      navigate("/");
+    }
+  }, [cookies, navigate]);
+
+  return (
+    <div className="tela-login">
+      <div className="logo">
+        <img src={logo} alt="Logo" />
+      </div>
+      <h2>
+        <span className="help-text">Help</span>
+        <span className="delphi-text">Delphi</span>
+      </h2>
+      <form onSubmit={onSubmit}>
+        <Input
+          label="Usuário"
+          name="user"
+          type="user"
+          value={user}
+          onChange={(e) => setUser(e.target.value)}
+        />
+        <Input
+          type="password"
+          name="password"
+          label="Senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <div className="button-container">
+          <Button type="submit">Acessar</Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Login from "./Login";
+import Login from "./Login2";
 import "./styles/index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "./contexts/AuthContext";
