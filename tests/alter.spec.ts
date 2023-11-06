a65e3f9228a82abb19c99aef5df01317f60f75a5
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2023-11-06 20:29:05 -0300

    fix: e2e test

--- tests/alter.spec.ts
+++ tests/alter.spec.ts
@@ -6,13 +6,18 @@ test.describe("alter status tests", () => {
 
     await page.getByTestId("open-modal").first().click();
 
-    await page.locator("select[name=status]").selectOption({ label: "Em andamento" });
+    await page
+      .locator("select[name=status]")
+      .selectOption({ label: "Em andamento" });
 
     await page.locator("button:has-text('Salvar')").click();
 
     await page.waitForLoadState("domcontentloaded");
 
-    const secondRowStatus = await page.locator("table tr:nth-child(2) td").nth(4).textContent();
+    const secondRowStatus = await page
+      .locator("table tr:nth-child(1) td")
+      .nth(4)
+      .textContent();
     console.log("Status na segunda linha:", secondRowStatus);
 
     await expect(secondRowStatus).toBe("Em Andamento");
